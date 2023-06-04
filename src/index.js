7852407714f70394fb8c6102d01d236c16cbd722
Vitor Luis Fernandes <vitor-l-f@hotmail.com>
2023-06-04 15:35:40 -0300

    feat: setting up base project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) => response.send("Hello World!!"));
+app.get('/', (request, response) => response.send('Hello World!!!'));
 
-app.listen(3000, () => console.log("Server started at http://localhost:3000"));
+app.listen(3000, () => console.log('Server started at http://localhost:3000'));
