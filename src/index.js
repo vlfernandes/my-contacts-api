e65cc621a0e3d16b1a49db5fd72ed5af56544171
Vitor Luis Fernandes <vitor-l-f@hotmail.com>
2023-06-04 15:14:38 -0300

    feat: start server created

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => response.send("Hello World!!"));
+
+app.listen(3000, () => console.log("Server started at http://localhost:3000"));
