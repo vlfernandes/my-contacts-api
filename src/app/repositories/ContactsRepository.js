8e8cc380053f698214c2f6660153a01659578526
Vitor Luis Fernandes <vitor-l-f@hotmail.com>
2023-06-05 00:43:05 -0300

    feat: create update and create api

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -19,17 +19,67 @@ let contacts = [
 
 class ContactsRepository {
   findAll() {
-    return new Promise((resolve) => { resolve(contacts); });
+    return new Promise((resolve) => {
+      resolve(contacts);
+    });
   }
 
   findById(id) {
-    return new Promise((resolve) => { resolve(contacts.find((contact) => contact.id === id)); });
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  findByEmail(email) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.email === email));
+    });
   }
 
   delete(id) {
     return new Promise((resolve) => {
       contacts = contacts.filter((contact) => contact.id !== id);
       resolve();
+    });
+  }
+
+  create({
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const newContact = {
+        id: v4(),
+        name,
+        email,
+        phone,
+        category_id,
+      };
+
+      contacts.push(newContact);
+      resolve();
+    });
+  }
+
+  update(id, {
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const updatedContact = {
+        id,
+        name,
+        email,
+        phone,
+        category_id,
+      };
+
+      contacts = contacts.map(
+        (contact) => (
+          contact.id === id
+            ? updatedContact
+            : contact),
+      );
+
+      resolve(updatedContact);
     });
   }
 }
